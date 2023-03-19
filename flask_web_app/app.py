5e2d6c08b02c0728901db3de3df825259be43618
Mårten Ward <Marten.ward@gmail.com>
2023-03-19 16:21:44 +0100

    fix(blogpost -> user)

--- flask_web_app/app.py
+++ flask_web_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from BlogPost.blogpost_model import BlogPost
+from models.user_model import User
 from Shared.supermodel import db
 from Utils.util import log_to_stdout
 
@@ -7,14 +7,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@db:5432/postgres"
 db.init_app(app) 
 
-with app.app_context():
-    db.create_all()
-
 @app.route('/')
 def index():
-    results = BlogPost.query.all()
-    serialised_result = BlogPost.serialise_list(results) 
+    results = db.session.query(User).all()
+    serialised_result = User.serialise_list(results) 
     return jsonify(serialised_result) 
+
 
 if __name__ == '__main__':
    app.run(debug=True, host="0.0.0.0")
---
+++ flask_web_app/models/user_model.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from Shared.supermodel import db
+from Shared.serialiser import Serialiser
+
+class User(db.Model, Serialiser):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return  "User with user_id %s" % str(self.id)
+
+    
