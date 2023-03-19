d40cc5888052ab51baae681bf4d29ef9c99acd44
Mårten Ward <Marten.ward@gmail.com>
2023-03-19 16:43:57 +0100

    add(sql_injection): blueprint

--- flask_web_app/app.py
+++ flask_web_app/app.py
@@ -2,10 +2,13 @@ from flask import Flask, jsonify
 from models.user_model import User
 from Shared.supermodel import db
 from Utils.util import log_to_stdout
+from blueprints.sql_injection import simple_page
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@db:5432/postgres"
 db.init_app(app) 
+app.register_blueprint(simple_page)
 
 @app.route('/')
 def index():
